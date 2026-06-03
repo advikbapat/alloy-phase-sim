4b3b308fe9558a7e979ee1f5a3a05f65b0c7d03b
advikbapat <advikbstudies@gmail.com>
2026-06-03 23:17:50 +0530

    added data in alloy_db and comments in thermodynamics.py

---
+++ core/thermodynamics.py
@@ -0,0 +1,20 @@
+
+# for a binary solution compute the gibbs energy of each phase
+# given 2 free energy curves, construct a common tangent to find the ends of the tie line
+# get a phase diagram
+
+"""
+def get_free_energy_curves(system, T, composition_range):
+    # Returns: dict of {phase_name: (x_array, G_array)}
+
+def get_phase_boundaries(system, T):
+    # Returns: list of (x_alpha, x_beta) tuples for each two-phase region
+
+def get_phase_diagram(system, T_range, composition_range):
+    # Returns: DataFrame with columns [T, phase, x_start, x_end]
+
+def get_equilibrium_phases(system, T, x):
+    # Returns: dict of {phase_name: phase_fraction} at a single (T, x) point
+    # This is what Member B needs for nucleation driving force
+"""
+
